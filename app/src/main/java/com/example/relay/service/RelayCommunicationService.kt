9d1cfaf5b64664e4d2dbd3ddf7a664f7d7703ded
NEGI46 <chuh48097@gmail.com>
2026-07-30 09:45:52 +0900

    style: wrap explicit PendingIntent intent

--- app/src/main/java/com/example/relay/service/RelayCommunicationService.kt
+++ app/src/main/java/com/example/relay/service/RelayCommunicationService.kt
@@ -143,7 +143,9 @@ class RelayCommunicationService : Service() {
     }
 
     private fun notification(connectedPeers: Int): Notification {
-        val stopIntent = Intent().setComponent(ComponentName(this, RelayCommunicationService::class.java)).setAction(ACTION_STOP)
+        val stopIntent = Intent()
+            .setComponent(ComponentName(this, RelayCommunicationService::class.java))
+            .setAction(ACTION_STOP)
         val stopPendingIntent = PendingIntent.getService(
             this,
             1,
